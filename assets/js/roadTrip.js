9e35dcb0f2cd054ded996caa26796166debb67ad
gmferland <gmferland@gmail.com>
2017-10-15 22:34:22 -0400

    fixed map so it renders, need to style

--- assets/js/roadTrip.js
+++ assets/js/roadTrip.js
@@ -1,5 +1,5 @@
 var width = 960;
-var height = 500;
+var height = 600;
 var visitedColor = 'rgb(69,173,168)';
 var defaultColor = '#d7d8da';
 var statesVisited = [
@@ -34,39 +34,27 @@ var statesVisited = [
   'Iowa',
 ];
 
-var projection = d3.geo.albersUsa()
-  .translate([width / 2, height / 2])
-  .scale([1000]);
-
-var path = d3.geo.path().projection(projection);
+var path = d3.geoPath();
 
 var svg = d3.select('body')
-  .append('svg')
+  .select('svg')
   .attr('viewBox', `0 0 ${width} ${height}`)
-  .attr('width', '100%')
-  .attr('height', 'auto');
+  .style('width', '100%')
+  .style('height', 'auto');
 
-d3.json('https://unpkg.com/us-atlas@1/us/10m.json', function(error, us) {
+d3.json('https://d3js.org/us-10m.v1.json', function(error, us) {
   if (error) throw error;
 
-  // Add my data to GeoJson data
-  var data = us.objects.states.features.map(function(geo) {
-    if (geo.properties.name !== 'Alaska' || geo.properties.name !== 'Hawaii') {
-      return Object.assign({ 'visited': statesVisited.indexOf(geo.properties.name) > -1 }, geo);
-    }
-  });
+  svg.append('g')
+    .attr('class', 'states')
+    .selectAll('path')
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter().append('path')
+    .attr('d', path);
 
-  svg.selectAll('path')
-    .data(data)
-    .enter()
-    .append('path')
-    .attr('d', path)
+  svg.append('path')
+    .attr('class', 'state-borders')
+    .attr('d', path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })))
     .style('stroke', '#fff')
-    .style('stroke-width', '1')
-    .style('fill', function(d) {
-      return d.visited ? visitedColor : defaultColor;
-    })
-    .on('hover', function(state) {
-      state.style('opacity', '0.7');
-    });
+    .style('stroke-width', '1');
 });
